80cf91c40294a5f6e5d2be063774e43be500230b
Christian Baier <christian.baier@stud-mail.uni-wuerzburg.de>
2018-07-28 20:55:37 +0200

    Usability features added (Loops)

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+pip install pandas numpy requests
+clear
+python ./src/main.py
--- src/main.py
+++ src/main.py
@@ -2,30 +2,38 @@ import os
 import monitor_connection
 import pandas as pd
 
-path = str(os.path.abspath(__file__)).replace("src/main.py", "").replace("src\\main.py", "")
+def run(URL, PW):
+    print("\nFollowing data is available:"+
+            "\n[1]---Past Year (Daily Data)"+
+            "\n[2]---Past Week (Hourly Data)"+
+            "\n[3]---Past Day  (10-minute-intervall Data)"+
+            "\n[4]---Past Hour (Minutely Data)\n\n")
+    dataset_index   = input("Please select dataset [1-4]:")
+    time_block      = blocks[dataset_index]
+    requestor       = monitor_connection.requestor(URL, PW)
+    dataset         = requestor.getData(time_block)
+    index           = dataset.index.tolist()
+    begin           = index[0]
+    end             = index[len(index) - 1]
+    begin_string    = str(begin.year) + "-" + str(begin.month) + "-" + str(begin.day) + "-" + str(begin.hour) + ":" + str(begin.minute)
+    end_string      = str(end.year) + "-" + str(end.month) + "-" + str(end.day) + "-" + str(end.hour) + ":" + str(end.minute)
 
-URL = "http://192.168.178.14"
+    dataset.to_csv(path_or_buf= path + time_block + "_" + begin_string + "_to_" + end_string + ".csv")
 
-blocks = {"1":"year", "2":"week", "3":"day", "4":"hour"}
+    if (input("Create another dataset?[y/n]") == "y"):
+        return True
+    else:
+        return False
 
-print("Welcome to Electro Log!\nFollowing data is available:"+
-        "\n[1]---Past Year (Daily Data)"+
-        "\n[2]---Past Week (Hourly Data)"+
-        "\n[3]---Past Day  (10-minute-intervall Data)"+
-        "\n[4]---Past Hour (Minutely Data)\n\n")
+path    = str(os.path.abspath(__file__)).replace("src/main.py", "").replace("src\\main.py", "")
+blocks  = {"1":"year", "2":"week", "3":"day", "4":"hour"}
 
-dataset_index = input("Please select dataset [1-4]:")
-time_block = blocks[dataset_index]
+print("----Welcome to Electro Log!----\n")
 
-PW = input("Please enter your password:")
+URL     = "http://192.168.178.14"
+PW      = input("Please enter your password:")
 
-requestor = monitor_connection.requestor(URL, PW)
-dataset = requestor.getData(time_block)
-index = dataset.index.tolist()
-begin = index[0]
-end = index[len(index) - 1]
+while(run(URL, PW) == True):
+    pass
 
-begin_string = str(begin.year) + "-" + str(begin.month) + "-" + str(begin.day) + "-" + str(begin.hour) + ":" + str(begin.minute)
-end_string = str(end.year) + "-" + str(end.month) + "-" + str(end.day) + "-" + str(end.hour) + ":" + str(end.minute)
 
-dataset.to_csv(path_or_buf= path + time_block + "_" + begin_string + "_to_" + end_string + ".csv")
